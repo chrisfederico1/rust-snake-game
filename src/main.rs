af6ff7ada62cb0a8581596c0f9edd477f638d028
chrisfederico1 <chris.federico@yahoo.com>
2023-04-20 14:07:27 -0500

    Initial commit of rust-snake-game

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use turtle::Drawing;
+
+fn main() {
+    let mut drawing = Drawing::new(); 
+    let _turtle = drawing.add_turtle();
+
+    drawing.set_size((640, 480));
+     
+
+
+
+
+
+    
+
+}
